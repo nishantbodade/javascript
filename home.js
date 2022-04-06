eb300be5b5322c5b2f3362bb2c00d4bd316fe89f
Nishant Bodade <nishantbodade@gmail.com>
2022-04-06 12:51:24 +0530

    Namming Variables

--- home.js
+++ home.js
@@ -1,20 +1,14 @@
-//single line comment
-let welcome='Welcome';  //single or double quets can be use
-let price=4.3;
-let discount=false;/*
-                            let welcome='Welcome,
-                                price=4.3,
-                                discount=false;        //shorhand
-                    */
+let welcome='Welcome',
+    price=4.3,
+    discount=false;        /*
+                                let is use to declare the variable
+                                _,$,latters can be use to define varibale at starting point
+                                _,$,latters,numbers can be followed
+                            */
+                            
 
 showMessage(welcome);
 
 
 
 
-
-/*
-console.log("any message")
-multilien comment block
-
-*/
