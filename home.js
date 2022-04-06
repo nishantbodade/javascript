d27aadbaa40438cb7201278e97b7080cda27bd70
Nishant Bodade <nishantbodade@gmail.com>
2022-04-06 16:30:55 +0530

    Booleab Variable

--- home.js
+++ home.js
@@ -1,12 +1,6 @@
-let msg=123;
-let amount="123";
-
-msg=msg.toString();
-amount=Number.parseInt(amount);
-
-
-//showMessage(typeof msg);
-showMessage(typeof amount);
+let saved=true
+saved=!saved;//invert
+showMessage(saved);
 
 
 
