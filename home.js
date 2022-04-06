d98e1f9175eaa3f04313da82e657155480a00e23
Nishant Bodade <nishantbodade@gmail.com>
2022-04-06 16:22:17 +0530

    String manipulation STring methods and properties

--- home.js
+++ home.js
@@ -1,8 +1,7 @@
+let msg="Nishant";
+msg=msg.length;//msg=msg.toUpperCase();
 
-let msg="Hello "; //"Hello \"Nishant "  output Hello "Nishant    will print the doubel qoutes ,escap character                         
-let name=`Nishant ${msg} `;  //backtick also use to create string  ,variable append in backtick
-
-showMessage(name);
+showMessage(msg);
 
 
 
