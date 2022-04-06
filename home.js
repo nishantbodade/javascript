2f367082d5dfdce7c4a48341945de9779aafc080
Nishant Bodade <nishantbodade@gmail.com>
2022-04-06 19:03:22 +0530

    Object Properties

--- home.js
+++ home.js
@@ -1,1 +1,9 @@
-changePercentOff(31);
+let person={
+    name:'Nishant',
+    age:20,
+    partTime:true
+};
+
+person['age']=33;//person.age=33  both same and can be use
+
+showMessage(person.age);
