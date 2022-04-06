a9e97c6931359ffe451dd455448c5be94f0b07c1
Nishant Bodade <nishantbodade@gmail.com>
2022-04-06 12:46:29 +0530

    declaring variables

--- home.js
+++ home.js
@@ -1,5 +1,16 @@
 //single line comment
-showMessage("Message");
+let welcome='Welcome';  //single or double quets can be use
+let price=4.3;
+let discount=false;/*
+                            let welcome='Welcome,
+                                price=4.3,
+                                discount=false;        //shorhand
+                    */
+
+showMessage(welcome);
+
+
+
 
 
 /*
