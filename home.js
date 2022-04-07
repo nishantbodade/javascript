4be14d1edc6d5f3ec7e4b6810463ca7f2870af3a
Nishant Bodade <nishantbodade@gmail.com>
2022-04-07 11:45:19 +0530

    Detecting Button Click

--- home.js
+++ home.js
@@ -1,3 +1,7 @@
-let header=document.getElementById('message');
-header.style.color='green';
-header.style.fontWeight='100';// use camel case in javascript ,font-Weight willl not work here, use FontWeight instead
+let button=document.getElementById('see-review');
+
+button.addEventListener('click',function(){  //annonimus function
+
+    console.log('click');
+
+})
