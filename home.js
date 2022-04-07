ef7a67ebf6c79882c2fe8e7482cffeddd58361c2
Nishant Bodade <nishantbodade@gmail.com>
2022-04-07 11:27:10 +0530

    Build-In Object javascript

--- home.js
+++ home.js
@@ -1,13 +1,6 @@
-let person={
-    name:'Nishant',
-    age:20,
-    partTime:true,
-    showInfo:function(realage){
-        showMessage(this.name+realage);
-    }
-};
-function incrementAge(p){
-    p.age++;
-}
-incrementAge(person)
-showMessage(person.age);
+let now=new Date();
+console.log(now.toDateString());
+console.log(Math.abs(-98));
+console.log(Math.random().toString());
+let s="hello";
+console.log(s.charAt(2));
