0e970d5a29519b8f16d60300008cc8b7d91d9dea
Nishant Bodade <nishantbodade@gmail.com>
2022-04-07 12:30:49 +0530

    slice and splice

--- home.js
+++ home.js
@@ -9,5 +9,10 @@ console.log(value);
 value.unshift(1);
 console.log(value);
 
-
+const newVal=value.slice(1,6);//slice will return new arrays, 
+console.log(newVal);
+value.splice(2,3);//deleting 
+console.log(value);
+value.splice(1,0,'xx');//inserting ,1 is index,0 is number of items to delete i.e. 0,values to add is 'xx' here 
+console.log(value);
 
