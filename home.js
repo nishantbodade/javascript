b15e427c6dcf7d0488b2aa886fdbeeab3e0ba1fc
Nishant Bodade <nishantbodade@gmail.com>
2022-04-07 12:43:33 +0530

    Array searching and looping

--- home.js
+++ home.js
@@ -1,18 +1,23 @@
 
-const value=Array.of(1,2,3);//const value=[1,2,3];   both are same
-value.push(4,5,6);
-console.log(value);
-value.pop();
-console.log(value);
-value.shift();
-console.log(value);
-value.unshift(1);
-console.log(value);
+const value=Array.of('a','b','cc');
+console.log(value.indexOf('b'));
 
-const newVal=value.slice(1,6);//slice will return new arrays, 
-console.log(newVal);
-value.splice(2,3);//deleting 
-console.log(value);
-value.splice(1,0,'xx');//inserting ,1 is index,0 is number of items to delete i.e. 0,values to add is 'xx' here 
-console.log(value);
+const set=value.filter(function(item){
+    console.log('item:',item);
 
+    return item>'b';
+})
+
+console.log(set);
+
+const find=value.find(function(item){
+    console.log('item:',item);
+
+    return item.length>1;
+})
+
+console.log(find);
+
+value.forEach(function(item){
+    console.log(item);
+})
